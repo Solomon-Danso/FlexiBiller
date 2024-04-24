7dd739fbef8664dc26824acb0cb5567a1ff29c60
Solomon <solomondanso2023@gmail.com>
2024-04-24 21:29:37 +0000

    Payment History Will Be Here

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { FooterComponent } from '../footer/footer.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,5 +11,13 @@ import { FooterComponent } from '../footer/footer.component';
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
+
+  constructor(private router: Router) {}
+
+paymentHistory(){
+  this.router.navigate(['/payment-history']);
+}
+
+
 
 }
